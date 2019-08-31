35fc9ca15c9e2bd2b5e26cd5abbaa6e0f593587c
irwin.jiangwei <17735528@qq.com>
2019-08-31 11:58:14 +0800

    new

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
